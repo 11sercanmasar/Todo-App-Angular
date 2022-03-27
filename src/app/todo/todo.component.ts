d39e06c80911d6339cc805a1a6280345d62a0485
sercan masar <smasar@outlook.com>
2022-03-27 09:48:43 +0300

    adding local storage and some functionality

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,23 +1,55 @@
+
 import { Component } from '@angular/core';
 import { Models } from 'src/models';
-
+import {TodoItem }from '../todoitem'
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent  {
-  constructor() { }
+  constructor() { 
+    this.model.items = this.getItemsFromLocalSt();
+  }
+
+  inputTxt: string = ""
 
   displayAll : boolean = false;
 
-  addItem(value:string){
-    if(value != "" ){
-    this.model.items.push( {  description :`${value}`, action: false })
+  addItem(){
+    if(this.inputTxt != "" ){
+    let data = {  description :`${this.inputTxt}`, action: false }
+    this.model.items.push(data);
+
+    let items = this.getItemsFromLocalSt();
+    items.push(data);
+
+    localStorage.setItem("items", JSON.stringify(items))
+    this.inputTxt = "";
     }
     else{ 
       alert("Please type a todo");
     }
+  }
+
+  getItemsFromLocalSt(){
+    let items:TodoItem[] = [];
+    let value   = localStorage.getItem("items");
+    if (value != null){
+      items = JSON.parse(value)
+    }
+    return items;
+  }
+
+  onActionChanged(item: TodoItem){
+   let items = this.getItemsFromLocalSt();
+   localStorage.clear();
+   items.forEach(i => {
+     if(i.description == item.description){
+       i.action = item.action;
+     }
+   })
+   localStorage.setItem("items", JSON.stringify(items))
   }
 
 
@@ -37,6 +69,15 @@ export class TodoComponent  {
 
   displayCount(){
     return this.model.items.filter(item => item.action) 
+  }
+
+  getBtnClasses(){
+    return { 
+      'disabled' : this.inputTxt.length == 0, 
+      'btn-secondary' : this.inputTxt.length == 0,
+      'btn-primary' : this.inputTxt.length > 0,
+      'text-white' : this.inputTxt.length > 0
+    }
   }
 
 }
